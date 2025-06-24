b151b152d6e1786e4a81d7fa8df40856c760a888
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-06-25 02:17:27 +0700

    /events/id

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,17 @@ app.get('/events',(req,res)=> {
     res.json(filteredEvents);
 })
 
+app.get('/events/:id',(req,res)=>{
+    const id = parseInt(req.params.id);
+    const event = events.find((event) => event.id === id);
+    if (event) {
+        res.json(event);
+    } else {
+        res.status(404).send('Event not found');
+    }
+})
+
+
 app.listen(port, () =>{
     console.log(`App listening at http://localhost:${port}`)
 })
