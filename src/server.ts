aff2de70aa37f2d560af2f7450c12e4b4e5a2ab4
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-06-25 00:41:09 +0700

    edit localhost

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/',(req: Request, res:Response)=>{
-    res.send('Hello World!')
+    let returnObj = {
+        name: 'John Doe',
+        age: 30,
+        occupation: 'Software Engineer'
+    }
+    res.send(returnObj)
 })
 
 app.listen(port, () =>{
-    console.log(`App listening at http://localhost${port}`)
+    console.log(`App listening at http://localhost:${port}`)
 })
