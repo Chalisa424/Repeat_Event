5c904bc27daa115d410eb963aaa3872a9a8e2e3d
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-06-25 14:06:56 +0700

    build endpoint post

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/',(req: Request, res:Response)=>{
     res.send('Hello World!')
 })
@@ -28,6 +30,12 @@ app.get('/events/:id',(req,res)=>{
     }
 })
 
+app.post('/events',(req,res) =>{
+  const newEvent : Event = req.body;
+  newEvent.id = events.length + 1;
+  events.push(newEvent);
+  res.json(newEvent);
+})
 
 app.listen(port, () =>{
     console.log(`App listening at http://localhost:${port}`)
